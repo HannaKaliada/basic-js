66b7990f7891430fb848f0d6831bb7f0c2e87ead
HannaKaliada <kaledaania2411@gmail.com>
2020-03-02 16:05:53 +0300

    refactor: improve algorithm

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,57 +1,28 @@
 class VigenereCipheringMachine {
-    constructor(isReverse) {
-        this.isReverse = isReverse;
+    constructor(reverse) {
+        this.reverse = reverse;
     }
     encrypt(message, key) {
         const lettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-        const numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
-        let encryptedArr = [];
-        let resultKeyArr = [];
-        let j = 0;
         let messageArr = message.toUpperCase().split('');
         let keyArr = key.toUpperCase().split('');
-        lettersToNum(messageArr);
-        lettersToNum(keyArr);
-        function lettersToNum(arr) {
-            for (let i = 0; i < arr.length; i++) {
-                for (let j = 0; j < lettersArr.length; j++) {
-                    if (arr[i] === lettersArr[j]) {
-                        arr[i] = numbersArr[j];
-                    }
-                }
-            }
-        }
-        for (let i = 0; resultKeyArr.length < messageArr.length; i++) {
-            if (typeof messageArr[j] != 'number') {
-                resultKeyArr.push('-');
-                i--;
-                j++;
-            }
-            else if (keyArr[i] == undefined) {
-                i = -1;
-            }
-            else {
-                j++;
-                resultKeyArr.push(keyArr[i]);
-            }
-        }
+        let encryptedArr = [];
+        messageArr = messageArr.map((letter) => {
+            return (lettersArr.indexOf(letter) == -1) ? letter : lettersArr.indexOf(letter);
+        })
+        keyArr = keyArr.map(letter => lettersArr.indexOf(letter));
+        let j = 0;
         for (let i = 0; i < messageArr.length; i++) {
-            if (typeof messageArr[i] != 'number') {
+            if (typeof (messageArr[i]) != 'number') {
                 encryptedArr.push(messageArr[i]);
+                continue;
             }
-            else {
-                encryptedArr.push(messageArr[i] + resultKeyArr[i]);
-                if (encryptedArr[i] > 26) {
-                    encryptedArr[i] = encryptedArr[i] - 26;
-                }
+            else if (keyArr[j] === undefined) {
+                j = 0;
             }
-        }
-        for (let i = 0; i < encryptedArr.length; i++) {
-            for (let j = 0; j < numbersArr.length; j++) {
-                if (typeof encryptedArr[i] == 'number' && encryptedArr[i] - 1 == numbersArr[j]) {
-                    encryptedArr[i] = lettersArr[j];
-                }
-            }
+
+            encryptedArr.push(lettersArr[(messageArr[i] + keyArr[j]) % 26]);
+            j++;
         }
         if (this.reverse == false) {
             return encryptedArr.reverse().join('');
@@ -63,54 +34,25 @@ class VigenereCipheringMachine {
 
     decrypt(encryptedMessage, key) {
         const lettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-        const numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
-        let decryptedArr = [];
-        let resultKeyArr = [];
         let messageArr = encryptedMessage.toUpperCase().split('');
         let keyArr = key.toUpperCase().split('');
+        let decryptedArr = [];
+        messageArr = messageArr.map((letter) => {
+            return (lettersArr.indexOf(letter) == -1) ? letter : lettersArr.indexOf(letter);
+        })
+        keyArr = keyArr.map(letter => lettersArr.indexOf(letter));
         let j = 0;
-        lettersToNum(messageArr);
-        lettersToNum(keyArr);
-        function lettersToNum(arr) {
-            for (let i = 0; i < arr.length; i++) {
-                for (let j = 0; j < lettersArr.length; j++) {
-                    if (arr[i] === lettersArr[j]) {
-                        arr[i] = numbersArr[j];
-                    }
-                }
-            }
-        }
-        for (let i = 0; resultKeyArr.length < messageArr.length; i++) {
-            if (typeof messageArr[j] != 'number') {
-                resultKeyArr.push('-');
-                i--;
-                j++;
-            }
-            else if (keyArr[i] == undefined) {
-                i = -1;
-            }
-            else {
-                j++;
-                resultKeyArr.push(keyArr[i]);
-            }
-        }
         for (let i = 0; i < messageArr.length; i++) {
-            if (typeof messageArr[i] != 'number') {
+            if (typeof (messageArr[i]) != 'number') {
                 decryptedArr.push(messageArr[i]);
+                continue;
             }
-            else {
-                decryptedArr.push(messageArr[i] - resultKeyArr[i]);
-                if (decryptedArr[i] < 0) {
-                    decryptedArr[i] = decryptedArr[i] + 26;
-                }
+            else if (keyArr[j] === undefined) {
+                j = 0;
             }
-        }
-        for (let i = 0; i < decryptedArr.length; i++) {
-            for (let j = 0; j < numbersArr.length; j++) {
-                if (typeof decryptedArr[i] == 'number' && decryptedArr[i] + 1 == numbersArr[j]) {
-                    decryptedArr[i] = lettersArr[j];
-                }
-            }
+
+            decryptedArr.push(lettersArr[(messageArr[i] + 26 - keyArr[j]) % 26]);
+            j++;
         }
         if (this.reverse == false) {
             return decryptedArr.reverse().join('');
